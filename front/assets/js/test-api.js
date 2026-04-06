d7ad84eef536d38dbbdabfe7f1eeea7e92df3cc6
prostiida <lunjevanatalja@gmail.com>
2026-04-06 18:14:29 +0300

    all profile ready

--- front/assets/js/test-api.js
+++ front/assets/js/test-api.js
@@ -1,9 +1,9 @@
 import { saveTestResult } from "/assets/js/api.js";
 
-export function saveTest(data){
-    if(!localStorage.getItem("token")) return;
+export function saveTest(data) {
+    if (!localStorage.getItem("token")) return;
 
     saveTestResult(data)
         .then(res => console.log("Saved:", res))
-        .catch(err => console.error(err));
+        .catch(err => console.error("Save test error:", err));
 }
